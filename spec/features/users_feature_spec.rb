e89514a3e2e742b103e9873e9499c87ce3463f91
Anindya Bhattacharyya <bat020@gmail.com>
2015-10-18 00:20:24 +0100

    adding devise features

---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'users:' do
+  before { visit '/' }
+  scenario 'can sign up' do
+    click_link 'sign up'
+    fill_in('Username', with: 'duchamp')
+    fill_in('Email', with: 'marcel@art.com')
+    fill_in('Password', with: 'rmutt')
+    fill_in('Password confirmation', with: 'rmutt')
+    click_button('SIGN UP')
+  end
+  scenario 'can log in' do
+
+  end
+  scenario 'can log out' do
+
+  end
+end
